4fd1aa6b99e79ebf805ca36e4dfd9e3b507d3ee7
ymj0828 <ymj369@nate.com>
2023-12-11 13:16:30 +0900

    style: Dropdown 컴포넌트 ui 생성

--- src/ui/Dropdown/Dropdown.jsx
+++ src/ui/Dropdown/Dropdown.jsx
@@ -1,5 +1,50 @@
-const Dropdown = () => {
-  
-}
+import { useState, useRef } from "react";
+import styles from "./Dropdown.module.css";
+import classNames from "classnames/bind";
+import { ReactComponent as IconArrowDown } from "../../icon/icon-arrow-down.svg";
 
-export default Dropdown
+const cx = classNames.bind(styles);
+
+const Dropdown = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState("이름순");
+  const dropdownRef = useRef(null);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleItemClick = (option) => {
+    setSelectedOption(option);
+    setIsOpen(false);
+  };
+
+  const handleClickOutside = (e) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
+      setIsOpen(false);
+    }
+  };
+
+  document.addEventListener("click", handleClickOutside);
+
+  return (
+    <div className={cx("container")} ref={dropdownRef}>
+      <button className={cx("button")} onClick={toggleDropdown}>
+        {selectedOption}
+        <IconArrowDown />
+      </button>
+      {isOpen && (
+        <ul className={cx("list")}>
+          <li className={cx("item")} onClick={() => handleItemClick("이름순")}>
+            이름순
+          </li>
+          <li className={cx("item")} onClick={() => handleItemClick("최신순")}>
+            최신순
+          </li>
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Dropdown;
