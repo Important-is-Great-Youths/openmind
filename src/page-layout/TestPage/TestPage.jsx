d69f74c56fd2cbdbbc21c46d191d23862c9d8552
youngeun <00eun1128@gmail.com>
2023-12-11 18:10:06 +0900

    fix : button box 수정중

--- src/page-layout/TestPage/TestPage.jsx
+++ src/page-layout/TestPage/TestPage.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import ButtonBox from "../../ui/ButtonBox/ButtonBox";
 
 export const TestPage = () => {
   return (
     <>
       <Link to="/">MainPage 가는 링크</Link>
+      <ButtonBox text={"입력하세요"} qnaBtn="questionBtn" />
     </>
   );
 };
--- src/ui/ButtonBox/ButtonBox.jsx
+++ src/ui/ButtonBox/ButtonBox.jsx
@@ -1,15 +1,16 @@
 import styles from "./ButtonBox.module.css";
 import { ReactComponent as ArrowRight2 } from "../../icon/icon-arrow-right2.svg";
+import classNames from "classnames/bind";
 
 export default function ButtonBox({ text, qnaBtn, qnaWidth }) {
   // const [isLoading, setIsLoading] = useState(false);
-
+  const cx = classNames.bind(styles);
   const cssQnaBtn = qnaBtn;
   return (
     <>
-      <button className={`${styles.qnaBtn} ${styles[cssQnaBtn]} ${styles[qnaWidth]}`}>
+      <button className={cx("qnaBtn", { cssQnaBtn }, { qnaWidth })}>
         <span>{text}</span>
-        <ArrowRight2 className={styles.arrowRight2} />
+        <ArrowRight2 className={cx("arrowRight2")} />
       </button>
     </>
   );
