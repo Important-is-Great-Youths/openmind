0a74e253b51fd0abd899878ef052fe6b1e36eade
ymj0828 <ymj369@nate.com>
2023-12-12 18:31:35 +0900

    style: App.js 불필요한 코드 삭제

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import { Outlet } from "react-router-dom";
-import InputTextarea from "./components/ui/InputTextarea/InputTextarea";
 import "./styles/global.css";
 
 function App() {
   return (
     <>
       <Outlet />
-      <InputTextarea />
     </>
   );
 }
