ebc92dba87ae7403fafb8e835605abf3308bbe8c
mini-chip <75009853+mini-chip@users.noreply.github.com>
2023-12-14 11:27:33 +0900

    Feat: 기능수정
    기능 수정 중

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Textarea from "./ui/InputTextarea/InputTextarea";
 
 function App() {
   return (
--- src/ui/Reaction/index.js
+++ src/ui/Reaction/index.js
@@ -1,1 +1,1 @@
-export default Reaction;
+export { default as Reaction } from "./Reaction";
