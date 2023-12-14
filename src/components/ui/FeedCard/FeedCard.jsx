c26f9fc62ff3d65462393de35b440255bc817723
youngeun <00eun1128@gmail.com>
2023-12-14 10:30:21 +0900

    feat : FeedCard 구현

---
+++ src/components/ui/FeedCard/FeedCard.jsx
@@ -0,0 +1,74 @@
+import styles from "./FeedCard.module.css";
+import classNames from "classnames/bind";
+import FeedCardAnswer from "../FeedCardAnswer/FeedCardAnswer";
+import FeedCardQuestion from "../FeedCardQuestion/FeedCardQuestion";
+import { ReactComponent as More } from "../../../icon/icon-more.svg";
+import { useState, useEffect } from "react";
+
+/* 
+more 버튼을 숨기고 싶은 경우,
+부모 컴포넌트에 다음 코드를 추가해주세요
+
+const [askFeed, setAskFeed] = useState(false);
+<FeedCard askFeed={askFeed} />
+*/
+
+export default function FeedCard({ askFeed }) {
+  const cx = classNames.bind(styles);
+  const [toggle, setToggle] = useState(false);
+  const [answerText, setAnswerText] = useState("");
+
+  const delAndRejectionHandler = (event) => {
+    event.stopPropagation();
+    setToggle(!toggle);
+  };
+
+  const unShowHandler = () => {
+    setToggle(false);
+  };
+
+  const rejectionBtnHandler = () => {
+    setToggle(false);
+    setAnswerText("답변거절");
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", unShowHandler);
+
+    return () => {
+      document.removeEventListener("click", unShowHandler);
+    };
+  }, []);
+
+  const answerStyle = {
+    color: answerText === "답변거절" ? "#B93333" : "inherit",
+  };
+
+  return (
+    <div className={cx("feedCard")}>
+      <div className={cx("feedTop")}>
+        <div>답변완료</div>
+        {askFeed && (
+          <div className={cx("delAndRejectionToggle")}>
+            <More className={cx("moreBtn")} onClick={delAndRejectionHandler} />
+            {toggle && (
+              <div className={cx("delAndRejection")}>
+                <div className={cx("rejection")} onClick={rejectionBtnHandler}>
+                  답변거절
+                </div>
+                <div className={cx("delete")}>질문삭제</div>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+      <FeedCardQuestion />
+      <FeedCardAnswer text={answerText} style={answerStyle} />
+      <i className={cx("feedBar")}></i>
+      <div>
+        <span>좋아요 </span>
+        <span>싫어요</span>
+      </div>
+    </div>
+  );
+}
