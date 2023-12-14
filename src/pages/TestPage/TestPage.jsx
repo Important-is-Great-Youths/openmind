eeb2a3daa88a9af823270a02fff7b2a555008b60
mini-chip <75009853+mini-chip@users.noreply.github.com>
2023-12-14 10:45:36 +0900

    feat : 기능 생성
    기능 생성 완료

--- src/pages/TestPage/TestPage.jsx
+++ src/pages/TestPage/TestPage.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import ButtonBox from "../../components/ui/ButtonBox/ButtonBox";
 import PostHeader from "../../components/feature/PostHeader/PostHeader";
-
+import Toast from "../../components/ui/Toast/Toast";
 export const TestPage = () => {
   return (
     <>
@@ -11,6 +11,7 @@ export const TestPage = () => {
       <h3>h3</h3>
       <ButtonBox text={"안녕"} qnaBtn="answerBtn" />
       <PostHeader />
+      <Toast />
     </>
   );
 };
