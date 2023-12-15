22d8358c6974355b6db495b570b329124a08d9d1
mini-chip <75009853+mini-chip@users.noreply.github.com>
2023-12-16 01:01:30 +0900

    feat: 기능 수정 중

--- src/ui/Reaction/Reaction.jsx
+++ src/ui/Reaction/Reaction.jsx
@@ -1,6 +1,6 @@
 import stylesheet from "./Reaction.module.css";
 import classNames from "classnames/bind";
-import { useState } from "react";
+import React, { useState } from "react";
 import { ReactComponent as Thumbup } from "../../icon/icon-thumbs-up.svg";
 import { ReactComponent as Thumbdown } from "../../icon/icon-thumbs-down.svg";
 
@@ -8,25 +8,33 @@ export default function Reaction() {
   const cx = classNames.bind(stylesheet);
   const [like, setLike] = useState(0);
   const [hate, setHate] = useState(0);
+  const [likeActive, setLikeActive] = useState(false);
+  const [hateActive, setHateActive] = useState(false);
+
+  const handleLikeClick = () => {
+    setLike(like + 1);
+    setLikeActive(!likeActive);
+  };
+
+  const handleHateClick = () => {
+    setHate(hate + 1);
+    setHateActive(!hateActive);
+  };
 
   return (
     <>
       <button
-        className={cx("buttondiv")}
-        onClick={() => {
-          setLike(like + 1);
-        }}
+        className={cx("buttondiv", { active: likeActive })}
+        onClick={handleLikeClick}
       >
-        <Thumbup className={cx("img")} />
+        <Thumbup className={cx("img", { active: likeActive })} />
         좋아요 {like}
       </button>
       <button
-        className={cx("buttondiv")}
-        onClick={() => {
-          setHate(hate + 1);
-        }}
+        className={cx("buttondiv", { active: hateActive })}
+        onClick={handleHateClick}
       >
-        <Thumbdown className={cx("img")} />
+        <Thumbdown className={cx("img", { active: hateActive })} />
         싫어요 {hate}
       </button>
     </>
