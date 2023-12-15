88cde15d356754eecd042178638eb5caf790f441
Lee Seo Young <seoyoung720@gmail.com>
2023-12-15 16:49:30 +0900

    chore : TestPage 정리 기존에 있던 사용 예시는 각 커스텀 hook로 이동해 주석 처리함

--- src/data-access/subjects/useGetSubject.js
+++ src/data-access/subjects/useGetSubject.js
@@ -9,3 +9,31 @@ export const useGetSubject = (subjectId) => {
 
   return { loading, error, data };
 };
+
+// 이하 사용 예시
+
+// const GetSubject = ({ subjectId }) => {
+//   const { loading, error, data } = useGetSubject(subjectId);
+
+//   if (loading) {
+//     return <p>Loading...</p>;
+//   }
+
+//   if (error) {
+//     return <p>Error: {error.message}</p>;
+//   }
+
+//   if (!data) {
+//     return <p>No data available.</p>;
+//   }
+
+//   // 여기서 data를 사용하여 원하는 렌더링을 수행
+//   return (
+//     <div>
+//       <h1>useGetSubject</h1>
+//       <p>name: {data.name}</p>
+//       <img src={data.imageSource} alt={data.name} />
+//       <p>questionCount: {data.questionCount}</p>
+//     </div>
+//   );
+// };
