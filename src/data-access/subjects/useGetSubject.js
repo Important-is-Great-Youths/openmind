51ad1ddf4eaf0ee1ebd34994e4134880becfc100
Lee Seo Young <seoyoung720@gmail.com>
2023-12-15 16:21:35 +0900

    fix : useGetSubject.js의 export 수정

--- src/data-access/subjects/useGetSubject.js
+++ src/data-access/subjects/useGetSubject.js
@@ -3,11 +3,9 @@ import { useAsync } from "../../util/useAsync";
 
 // subjectId를 받아 답변자의 id, name, imageSource, questionCount, createdAt를 가져오는 Hook
 
-const useGetSubject = (subjectId) => {
+export const useGetSubject = (subjectId) => {
   const getSubject = () => axiosInstance.get(`subjects/${subjectId}/`);
   const { loading, error, data } = useAsync(getSubject);
 
   return { loading, error, data };
 };
-
-export default useGetSubject;
