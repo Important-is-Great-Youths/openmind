09671560b4f1bbe4cabeb38a1f7c403a77c3e306
Lee Seo Young <seoyoung720@gmail.com>
2023-12-16 10:34:26 +0900

    chore : useDeleteSubject.js에 사용 예시 주석 추가

--- src/data-access/subjects/useDeleteSubject.js
+++ src/data-access/subjects/useDeleteSubject.js
@@ -23,3 +23,23 @@ export const useDeleteSubject = () => {
 
   return { loading, error, deleteData, deleteSubject };
 };
+
+// 이하 사용 예시
+// const DeleteSubjectButton = ({ subjectId }) => {
+//   const { loading, error, deleteData, deleteSubject } = useDeleteSubject();
+
+//   const handleDelete = () => {
+//     deleteSubject(subjectId);
+//   };
+
+//   return (
+//     <div>
+//       <button onClick={handleDelete} disabled={loading}>
+//         Delete Subject
+//       </button>
+//       {loading && <p>Loading...</p>}
+//       {error && <p>Error: {error.message}</p>}
+//       {deleteData && <p>Data Deleted: {JSON.stringify(deleteData)}</p>}
+//     </div>
+//   );
+// };
