96fa7ff68995ace8b5f0a79fa488d3e6a9fa2590
mini-chip <75009853+mini-chip@users.noreply.github.com>
2023-12-18 00:58:34 +0900

    Update TestPage.jsx

--- src/page-layout/TestPage/TestPage.jsx
+++ src/page-layout/TestPage/TestPage.jsx
@@ -1,10 +1,8 @@
 import { Link } from "react-router-dom";
-import { Reaction } from "../../ui/Reaction";
 export const TestPage = () => {
   return (
     <>
       <Link to="/">MainPage 가는 링크</Link>
-      <Reaction />
     </>
   );
 };
