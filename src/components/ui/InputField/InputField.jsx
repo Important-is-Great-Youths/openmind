e14cdcef7ca9962039568ad86b35b29b3e5a418b
muyahho <hoodysonyeo@gmail.com>
2023-12-17 18:27:16 +0900

    feat: save temporary work status

--- src/components/ui/InputField/InputField.jsx
+++ src/components/ui/InputField/InputField.jsx
@@ -1,16 +1,40 @@
+// InputField.jsx
+
+import React, { useState } from 'react';
+import { usePostSubjects } from './usePostSubjects';
 import styles from "./InputField.module.css";
 import classNames from "classnames/bind";
 import { ReactComponent as IconPerson } from "../../../icon/icon-person.svg";
 
 const cx = classNames.bind(styles);
 
-const InputField = ({value, onChange}) => {
-  return (
-    <div className={cx("input-wrap")}>
-      <input className={cx("input")} type="text" placeholder="이름을 입력하세요" value={value} onChange={onChange} />
-      <IconPerson className={cx("icon")} />
-    </div>
-  );
+const InputField = () => {
+    const [name, setName] = useState('');
+    const { postSubject } = usePostSubjects();
+
+    const handleInputChange = (e) => {
+        setName(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const subject = postSubject(name);
+        console.log('Subject ID:', subject.id);
+    };
+
+    return (
+        <form className={cx("input-wrap")} onSubmit={handleSubmit}>
+            <input
+                className={cx("input")}
+                type="text"
+                placeholder="이름을 입력하세요"
+                value={name}
+                onChange={handleInputChange}
+            />
+            <IconPerson className={cx("icon")} />
+            <button type="submit">Submit</button>
+        </form>
+    );
 };
 
 export default InputField;
