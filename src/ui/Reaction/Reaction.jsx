1c9b9f1ed8bafb335710a42c5abcefeec8453a5c
mini-chip <75009853+mini-chip@users.noreply.github.com>
2023-12-18 00:32:28 +0900

    feat: 새로고침기능수정
    새로고침했을 때 0으로 초기화되는 문제 수정

--- src/ui/Reaction/Reaction.jsx
+++ src/ui/Reaction/Reaction.jsx
@@ -1,40 +1,40 @@
 import stylesheet from "./Reaction.module.css";
 import classNames from "classnames/bind";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { ReactComponent as Thumbup } from "../../icon/icon-thumbs-up.svg";
 import { ReactComponent as Thumbdown } from "../../icon/icon-thumbs-down.svg";
 
 export default function Reaction() {
   const cx = classNames.bind(stylesheet);
-  const [like, setLike] = useState(0);
-  const [hate, setHate] = useState(0);
-  const [likeActive, setLikeActive] = useState(false);
-  const [hateActive, setHateActive] = useState(false);
+  const initialCountLike = parseInt(localStorage.getItem("like")) || 0;
+  const initialCountHate = parseInt(localStorage.getItem("hate")) || 0;
+  const [like, setLike] = useState(initialCountLike);
+  const [hate, setHate] = useState(initialCountHate);
 
   const handleLikeClick = () => {
     setLike(like + 1);
-    setLikeActive(!likeActive);
   };
 
   const handleHateClick = () => {
     setHate(hate + 1);
-    setHateActive(!hateActive);
   };
+
+  useEffect(() => {
+    localStorage.setItem("like", like.toString());
+  }, [like]);
+
+  useEffect(() => {
+    localStorage.setItem("hate", hate.toString());
+  }, [hate]);
 
   return (
     <>
-      <button
-        className={cx("buttondiv", { active: likeActive })}
-        onClick={handleLikeClick}
-      >
-        <Thumbup className={cx("img", { active: likeActive })} />
+      <button className={cx("buttondiv")} onClick={handleLikeClick}>
+        <Thumbup className={cx("img")} />
         좋아요 {like}
       </button>
-      <button
-        className={cx("buttondiv", { active: hateActive })}
-        onClick={handleHateClick}
-      >
-        <Thumbdown className={cx("img", { active: hateActive })} />
+      <button className={cx("buttondiv")} onClick={handleHateClick}>
+        <Thumbdown className={cx("img")} />
         싫어요 {hate}
       </button>
     </>
