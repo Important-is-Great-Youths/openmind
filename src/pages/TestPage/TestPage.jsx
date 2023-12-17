d8c09970540e8eded480dc849a9c6228fb27445c
Lee Seo Young <seoyoung720@gmail.com>
2023-12-17 17:26:50 +0900

    feat : useDeleteAnswer.js 생성 및 시험 적용

--- src/pages/TestPage/TestPage.jsx
+++ src/pages/TestPage/TestPage.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { useGetAnswer } from "../../data-access/answers/useGetAnswer";
+import { usePutAnswer } from "../../data-access/answers/usePutAnswer";
 
 const GetAnswer = ({ answerId }) => {
   const { loading, error, data } = useGetAnswer(answerId);
@@ -26,16 +27,63 @@ const GetAnswer = ({ answerId }) => {
       <p>id: {answer.id}</p>
       <p>questionId: {answer.questionId}</p>
       <p>content: {answer.content}</p>
-      <p>isRejected: {answer.isRejected}</p>
+      <p>isRejected: {String(answer.isRejected)}</p>
       <p>createdAt: {answer.createdAt}</p>
     </div>
   );
 };
 
+const PutAnswerForm = ({ answerId }) => {
+  const [content, setContent] = useState("");
+  const [isRejected, setIsRejected] = useState(false);
+
+  const { loading, error, putData, putAnswer } = usePutAnswer();
+
+  const handlePutAnswer = async (e) => {
+    e.preventDefault();
+    try {
+      await putAnswer(answerId, content, isRejected);
+    } catch (error) {
+      console.error("Error while updateing answer: ", error);
+    }
+  };
+
+  return (
+    <form onSubmit={handlePutAnswer}>
+      <label>
+        Content :
+        <input
+          type="text"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        IsRejected :
+        <input
+          type="checkbox"
+          checked={isRejected}
+          onChange={(e) => setIsRejected(e.target.checked)}
+        />
+      </label>
+      <br />
+      <button type="submit" disabled={loading}>
+        {loading ? "Updating..." : "Update Answer"}
+      </button>
+      {error && <p>Error: {error.message}</p>}
+      {putData && <p>Answer updated successfully!</p>}
+    </form>
+  );
+};
+
 export const TestPage = () => {
+  const ANSWER_ID = "1590";
   return (
     <>
-      <GetAnswer answerId="1590" />
+      <GetAnswer answerId={ANSWER_ID} />
+      <br />
+      <PutAnswerForm answerId={ANSWER_ID} />
     </>
   );
 };
