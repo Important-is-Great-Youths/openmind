fd6c9afc593cf06727927d73cfaeb187916e0cb3
Lee Seo Young <seoyoung720@gmail.com>
2023-12-19 17:14:59 +0900

    fix : ButtonBox, Badge 코드 혼동 사태 해결

--- src/components/ui/ButtonBox/ButtonBox.jsx
+++ src/components/ui/ButtonBox/ButtonBox.jsx
@@ -1,21 +1,42 @@
-// Badge.jsx
-
-import React from "react";
 import styles from "./ButtonBox.module.css";
+import { ReactComponent as ArrowNext } from "../../../icon/icon-arrow-next.svg";
 import classNames from "classnames/bind";
 
-const cs = classNames.bind(styles);
+/* 질문 받기 (화살표없음)이 기본 style로 적용됩니다.
+qnaBtn = "answerBtn"  답변하러가기 -> (화살표 있음) 버튼 style이 적용됩니다.
 
-const Badge = ({ Completed }) => {
-    const badgeClass = cs({
-            badge: true,
-            answer: Completed,
-            unanswered: !Completed,
-            //추가적인 클래스들
-        }
+추가 style 적용하려면 아래 클래스명을 추가하세요.
+width 100% : qnaWidth="qnaWidth"
+비활성상태 : qnaInactivate = "qnaInactivate" 
+*/
 
-    );
-    return <div className={badgeClass}>{Completed ? "답변완료" : "미답변"}</div>;
-};
+export default function ButtonBox({
+  text,
+  qnaBtn,
+  qnaWidth,
+  qnaInactivate,
+  onClick,
+  onClose,
+}) {
+  const cx = classNames.bind(styles);
+  const cssQnaBtn = qnaBtn;
 
-export default Badge;
+  const handleButtonClick = () => {
+    onClick && onClick();
+    onClose && onClose();
+  };
+
+  return (
+    <>
+      <button
+        className={cx("qnaBtn", cssQnaBtn, qnaWidth, qnaInactivate, {
+          noArrow: !cssQnaBtn,
+        })}
+        onClick={handleButtonClick}
+      >
+        <span>{text}</span>
+        {cssQnaBtn && <ArrowNext className={cx("arrowNext")} />}
+      </button>
+    </>
+  );
+}
