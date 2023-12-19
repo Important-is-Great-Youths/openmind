6dfe3a3b907434206d3c81af6569c2e8894ce3ec
ymj0828 <ymj369@nate.com>
2023-12-19 17:51:27 +0900

    feat: 데이터 전달

--- src/layout/BasePostLayout.jsx
+++ src/layout/BasePostLayout.jsx
@@ -1,20 +1,14 @@
 import PostHeader from "../components/feature/PostHeader/PostHeader";
 import classNames from "classnames/bind";
 import styles from "./BasePostLayout.module.css";
-import ButtonFloating from "../components/ui/ButtonFloating/ButtonFloating";
 
 const cx = classNames.bind(styles);
 
-const BasePostLayout = ({ children }) => {
+const BasePostLayout = ({ children, id }) => {
   return (
     <>
-      <PostHeader />
-      <main className={cx("main")}>
-        {children}
-        <div className={cx("button")}>
-          <ButtonFloating className={cx("button")} text={"질문 작성하기"} />
-        </div>
-      </main>
+      <PostHeader id={id} />
+      <main className={cx("main")}>{children}</main>
     </>
   );
 };
