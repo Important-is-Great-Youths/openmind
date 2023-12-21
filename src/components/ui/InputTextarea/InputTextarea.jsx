74ab561ade123fe641d8834e0f9dd01477eb5e3c
mini-chip <75009853+mini-chip@users.noreply.github.com>
2023-12-21 11:08:59 +0900

    update: InputTextarea 재업로드
    재업로드

--- src/components/ui/InputTextarea/InputTextarea.jsx
+++ src/components/ui/InputTextarea/InputTextarea.jsx
@@ -3,22 +3,17 @@ import classNames from "classnames/bind";
 
 const cx = classNames.bind(stylesheet);
 
-const focusing = (e) => {
-  // 필요한 작업을 수행
-};
-
 export default function InputTextarea({ text, value, onChange }) {
   return (
-   <form className={cx("inputarea")}>
-    <textarea
-      cols="70"
-      rows="7"
-      className={cx("input")}
-      placeholder={text}
-      onFocus={focusing}
-      value={value}
-      onChange={onChange}
-    />
-  </form>
+    <form className={cx("inputarea")}>
+      <textarea
+        cols="70"
+        rows="7"
+        className={cx("input")}
+        placeholder={text}
+        value={value}
+        onChange={onChange}
+      />
+    </form>
   );
 }
