29a99c7625850cff26d23d21a5d4b0b7f99641f0
youngeun <00eun1128@gmail.com>
2024-01-15 23:05:38 +0900

    feat : ButtonFloating typescript로 마이그레이션

--- src/components/ui/ButtonFloating/ButtonFloating.tsx
+++ src/components/ui/ButtonFloating/ButtonFloating.tsx
@@ -1,11 +1,18 @@
-import styles from "./ButtonFloating.module.css";
+import styles from "./ButtonFloating.module.scss";
 import classNames from "classnames/bind";
 
 /* 
   삭제하기(작은 사이즈의 버튼)을 쓸때 
   <ButtonFloating text={"삭제하기"} small="small" />
 */
-export default function ButtonFloating({ text, small, onClick }) {
+
+interface Props {
+  text: string;
+  small?: "small" | undefined;
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+export default function ButtonFloating({ text, small, onClick }: Props) {
   const cx = classNames.bind(styles);
   return (
     <>
